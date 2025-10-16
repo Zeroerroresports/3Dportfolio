7cc71c179e788319603951646ea0525299080e92
Priyesh Kekan <kekanpriyesh03@gmail.com>
2025-10-16 17:43:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,27 @@
 import React from "react";
-import Tilt from 'react-parallax-tilt';
-
+import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
-
 import { styles } from "../styles";
 import { services } from "../constants";
-import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <Tilt
+    tiltMaxAngleX={45}
+    tiltMaxAngleY={45}
+    scale={1.05}
+    transitionSpeed={450}
+    className="xs:w-[250px] w-full"
+  >
     <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
-        tiltMaxAngleX={45} tiltMaxAngleY={45} scale={1.05} transitionSpeed={450}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
-
-        <h3 className='text-white text-[20px] font-bold text-center'>
-          {title}
-        </h3>
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
       </div>
     </motion.div>
   </Tilt>
@@ -42,12 +37,15 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a passionate Game and Frontend Developer with hands-on experience in creating interactive and visually engaging projects using Unity, React, and JavaScript. I enjoy blending creativity with technical problem-solving to design smooth gameplay experiences and responsive web interfaces.
+        I’m a passionate developer skilled in creating interactive 3D experiences,
+        modern web applications, and efficient workflows using tools like
+        React, Three.js, and Tailwind CSS. I love crafting seamless, responsive,
+        and visually stunning projects that merge creativity with functionality.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
@@ -56,6 +54,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, "about");
-
-
+export default About;
